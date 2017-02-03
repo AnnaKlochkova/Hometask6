97e3612623168c3b47c7e8959d4988aed90dceaa
Anna <a.n.klochkova1@gmail.com>
2017-02-04 01:33:06 +0200

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,20 @@
+package task2;
+
+public class Task2 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a= 5;
+double b =1.2;
+String s = "apple";
+System.out.println (con(a,b,s));
+	}
+
+	public static String con (int a,double b, String s) {
+		double con = (a+b);
+		return  s + con;
+	} 
+}
+	
+
